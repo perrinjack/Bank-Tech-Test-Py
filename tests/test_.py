7e70c55ba7b9935acc9e7f3054bc0dbb10c454cd
J Perrin <perrinjack96@gmail.com>
2020-07-16 12:00:44 +0100

    added error messages to tests

--- tests/test_.py
+++ tests/test_.py
@@ -1,21 +1,27 @@
 from lib.account import Account
 
 class TestClass:
+
+    #Account Class
     
     def test_one(self):
         account = Account()
-        assert account.balance == 0
+        assert account.balance == 0, "Account should start with 0 balance"
 
     def test_two(self):
         account = Account()
-        assert account.transactions == []
+        assert account.transactions == [], "Account should start with an empty transacton list"
 
     def test_three(self):
         account = Account()
         account.credit(100)
-        assert account.balance == 100
+        assert account.balance == 100, "Creditting an account should increase balance by amount"
 
     def test_four(self):
          account = Account()
          account.withdraw(100)
-         assert account.balance == -100
+         assert account.balance == -100, "withdrawing from an account should decrease balance by amount"
+
+
+
+    #Transaction Class

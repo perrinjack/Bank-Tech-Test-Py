00ddbc5fa883f861cb68e0a7703f1d96f6bb74f1
J Perrin <perrinjack96@gmail.com>
2020-07-16 12:29:40 +0100

    GREEN, transaction has the balacne of account after settlement

--- lib/transaction.py
+++ lib/transaction.py
@@ -1,5 +1,6 @@
 class Transaction:
 
-    def __init__(self, value, type):
+    def __init__(self, value, type, current_balance):
         self.value = value
         self.type = type
+        self.current_balance = current_balance

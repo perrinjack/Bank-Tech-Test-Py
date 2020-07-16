841a526160f5ca5e24bdf19fbdf5902d99cd6d0c
J Perrin <perrinjack96@gmail.com>
2020-07-16 12:20:57 +0100

    RED each transaction has a type

--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -4,4 +4,11 @@ class Test_TransactionClass:
 
     def test_stores_transaction_value(self):
         transaction = Transaction(300)
-        assert transaction.value == 300
+        assert transaction.value == 300, 'transaction has a transaction value'
+
+    def test_stores_transaction_type(self):
+        transaction_cred = Transaction(300, 'credit')
+        assert transaction_cred.type == 'credit'
+
+
+    
